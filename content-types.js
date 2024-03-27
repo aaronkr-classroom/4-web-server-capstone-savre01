0590ab0ca299b2fba957fd45576fff926b73685f
ouhyeon <163230719+savre01@users.noreply.github.com>
2024-03-27 06:22:48 +0000

    web app

---
+++ content-types.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = {
+    html: {
+        "Content-Type": "text/html"
+    },
+    text: {
+        "Content-Type": "text/plain"
+    },
+    css: {
+        "Content-Type": "text/css"
+    },
+    js: {
+        "Content-Type": "text/javascript"
+    },
+    jpg: {
+        "Content-Type": "image/jpg"
+    },
+    png: {
+        "Content-Type": "image/png"
+    },
+}
